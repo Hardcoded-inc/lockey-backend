0a97ed1de9d8229a6e10205694bb2a02d08295ee
Oskar Legner <oskarlegner@gmail.com>
2023-01-14 16:53:37 +0100

    Use App Settings for keeping secrets in cloud

--- UserEndpoint/__init__.py
+++ UserEndpoint/__init__.py
@@ -1,6 +1,5 @@
 import azure.functions as func
 import pyodbc
-from dotenv import dotenv_values
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -8,14 +7,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     print("-------")
     print(req.params)
 
-    # Set up connection to SQL database
-    env_values = dotenv_values()
 
-    server = env_values["SERVER"]
-    driver = env_values["DRIVER"]
-    database = env_values["DATABASE"]
-    username = env_values['DB_USERNAME']
-    password = env_values['DB_PASSWORD']
+    server = os.environ.get("SERVER")
+    driver = os.environ.get("DRIVER")
+    database = os.environ.get("DATABASE")
+    username = os.environ.get('DB_USERNAME')
+    password = os.environ.get('DB_PASSWORD')
 
     connection_string = 'DRIVER='+driver+';' \
                         'SERVER='+server+';' \
