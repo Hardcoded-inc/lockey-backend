ef3d637d6a4cff4154076502fe1e90723d1a10b6
Oskar Legner <oskarlegner@gmail.com>
2023-01-17 23:52:48 +0100

    Make door opening work

--- OpenDoor/__init__.py
+++ OpenDoor/__init__.py
@@ -13,37 +13,24 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
 
 
-def create_door(body: bytes) -> func.HttpResponse:
+def open_door(id: int) -> func.HttpResponse:
     connection = db.get_connection()
     cursor = connection.cursor()
 
-    cursor.execute(
-        'SELECT is_open FROM doors WHERE id = ?',
-        id
-    )
-    door = cursor.fetchone()
+    cursor.execute('SELECT is_open FROM doors WHERE id = ?', id)
+    door = cursor.fetchone() # returns not dict, but tuple: (is_open, )... ಠ_ಠ
 
-    if not door["is_open"]:
-        cursor.execute('UPDATE doors SET is_open = true WHERE id = ?', id)
+    if door == None:
+        return func.HttpResponse("Not found", status_code=404)
 
-        # Set timer and lock door again after 10 sek
+    elif not door[0]:
+        cursor.execute('UPDATE dbo.doors SET is_open = \'True\' WHERE id = ?', id)
+        connection.commit()
+        # TODO: Set timer and lock door again after 10 sek
+        return func.HttpResponse(f"Door openned successfully.")
 
+    else:
+        return func.HttpResponse(f"Door is already open.")
 
-
-#     user = cursor.fetchone()
-#
-#     if (not user):
-#         return func.HttpResponse(json.dumps({"error": "User not found"}), status_code=404, mimetype="application/json")
-#
-#     return func.HttpResponse(json.dumps(tuple(user), default=str), mimetype="application/json")
-#
-#
-#
-#
-#     return func.HttpResponse(
-#             "Bad data",
-#             status_code=400
-#     )
-#
 
 
