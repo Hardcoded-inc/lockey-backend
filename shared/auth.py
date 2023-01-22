270ad4fb02f89646c06e3ae807763e8c36d523f1
Oskar Legner <oskarlegner@gmail.com>
2023-01-22 22:29:38 +0100

    Add helpful comment

--- shared/auth.py
+++ shared/auth.py
@@ -2,10 +2,10 @@ import logging
 from shared import db, jwt, cookie
 import azure.functions as func
 
-
 def auth(req):
     token = cookie.read(req, "jwt")
     auth_data = jwt.decode(token)
     logging.warn(auth_data)
+    # {'id': 1, 'username': 'FirstUser', 'is_admin': False}
 
     return auth_data
