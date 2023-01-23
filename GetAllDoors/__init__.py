387365845685dde557969d1867af609c4bda09b8
Oskar Legner <oskarlegner@gmail.com>
2023-01-23 20:43:34 +0100

    Fix doors listing

--- GetAllDoors/__init__.py
+++ GetAllDoors/__init__.py
@@ -1,27 +1,16 @@
 import logging
 from shared import db
-from shared.auth import auth
 import azure.functions as func
 import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Get all doors function processed a request.')
-    session = auth(req)
 
-    get_all_doors(session)
-
-def get_all_doors():
     conn = db.get_connection();
     cursor = conn.cursor();
 
-    id = None if session["is_admin"] else session["id"]
-    if id:
-        # TODO: Add querying doors for specific user
-        cursor.execute('SELECT * FROM doors')
-    else:
-        cursor.execute('SELECT * FROM doors')
-
+    cursor.execute('SELECT * FROM doors')
     doors = cursor.fetchall()
     results = [tuple(row) for row in doors]
 
