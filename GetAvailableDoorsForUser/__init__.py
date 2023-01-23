2db5f98daa4071d4bdc4a5e50cb67cfabdc9e6a9
sandis-jakub-m <j.macioszek@sandis.io>
2023-01-23 22:28:54 +0100

    Add endpoint for user availble doors

---
+++ GetAvailableDoorsForUser/__init__.py
@@ -0,0 +1,23 @@
+import logging
+from shared import db
+import azure.functions as func
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Get all doors function processed a request.')
+    id = req.route_params.get('id')
+
+    user = db.query_one('SELECT * FROM dbo.users WHERE id = ?', id)
+
+    if (not user):
+        return func.HttpResponse(json.dumps({"error": "User not found"}), status_code=404, mimetype="application/json")
+
+    user['available_doors'] = db.query_all(build_query(), id)
+
+    return func.HttpResponse(json.dumps(user, default=str), mimetype="application/json")
+
+def build_query():
+    return '''SELECT * FROM doors AS d
+                LEFT OUTER JOIN users_doors AS ud ON d.id = ud.door_id
+                WHERE ud.user_id IS NULL OR ud.user_id != ?'''
