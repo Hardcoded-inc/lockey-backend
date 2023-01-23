086fec7cc606557c319e2d789eddb4fb65b498f0
Oskar Legner <oskarlegner@gmail.com>
2023-01-24 00:31:53 +0100

    Add doors listing nested for each user

--- GetAllUsers/__init__.py
+++ GetAllUsers/__init__.py
@@ -7,15 +7,34 @@ import json
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    users = db.query_all('SELECT u.id, u.username, u.is_admin FROM users u')
-#
-#
-#     cursor.execute(
-#             'SELECT d.id, d.name, d.is_open FROM doors'
-#             'LEFT JOIN users_doors ud ON u.id = ud.user_id '
-#             'LEFT JOIN doors d ON d.id = ud.door_id '
-#
-#         )
-#     users = cursor.fetchall()
+    users = db.query_all('SELECT u.id, u.username, ud.door_id FROM users u LEFT JOIN users_doors ud ON u.id = ud.user_id')
+    users_with_doors = list(filter(lambda u: u["door_id"], users))
+    users_without_doors = list(filter(lambda u: not u["door_id"], users))
+
+    for u in users_without_doors: u["doors"] = []
+    users = users_without_doors
+
+    for user in users_with_doors:
+        door = db.query_one("SELECT * FROM doors WHERE id=?", user["door_id"])
+        try:
+            idx = list(u["id"] for u in users).index(user["id"])
+            users[idx]["doors"].append(door)
+
+        except Exception as e:
+            users.append(user)
+            users[-1]["doors"] = []
+            users[-1]["doors"].append(door)
+
+    users = list(removekey(u, "door_id") for u in users)
+    logging.warn(users)
 
     return func.HttpResponse(json.dumps(users, default=str), mimetype="application/json")
+
+
+
+
+
+def removekey(d, key):
+    r = dict(d)
+    del r[key]
+    return r
