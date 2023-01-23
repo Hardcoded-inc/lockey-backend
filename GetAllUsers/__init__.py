ae5ae82e0306b2a11822db64e00fb2e1d605dbe1
Oskar Legner <oskarlegner@gmail.com>
2023-01-23 21:02:08 +0100

    Update result json format

--- GetAllUsers/__init__.py
+++ GetAllUsers/__init__.py
@@ -7,13 +7,25 @@ import json
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    users = query('SELECT u.id, u.username, u.is_admin FROM users u')
+#
+#
+#     cursor.execute(
+#             'SELECT d.id, d.name, d.is_open FROM doors'
+#             'LEFT JOIN users_doors ud ON u.id = ud.user_id '
+#             'LEFT JOIN doors d ON d.id = ud.door_id '
+#
+#         )
+#     users = cursor.fetchall()
+
+    return func.HttpResponse(json.dumps(users, default=str), mimetype="application/json")
+
+
+
+
+def query(query, params=None):
     conn = db.get_connection();
     cursor = conn.cursor();
-
-    cursor.execute(
-            'SELECT * FROM users',
-        )
-    users = cursor.fetchall()
-    results = [tuple(row) for row in users]
-    
-    return func.HttpResponse(json.dumps(results, default=str), mimetype="application/json")
+    cursor.execute(query)
+    return [dict(zip([column[0] for column in cursor.description], row))
+             for row in cursor.fetchall()]
