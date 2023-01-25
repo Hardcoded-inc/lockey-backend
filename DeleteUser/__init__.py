b7ac64ee598568c692508792d2bcca7191a93037
Oskar Legner <oskarlegner@gmail.com>
2023-01-26 00:25:54 +0100

    Fix auth guard

--- DeleteUser/__init__.py
+++ DeleteUser/__init__.py
@@ -9,17 +9,15 @@ from shared.auth import auth, AuthLevels
 def main(req: func.HttpRequest) -> func.HttpResponse:
     id = req.route_params.get('id')
     logging.info(f"Delete User (id: {id}) function processed a request.")
-    auth_data = auth(req, AuthLevels.ADMIN)
 
-    return delete_user(id)
-
+    return auth(req, lambda d: delete_user(id), AuthLevels.ADMIN)
 
 
 def delete_user(id: int) -> func.HttpResponse:
     connection = db.get_connection()
     cursor = connection.cursor()
     cursor.execute('DELETE FROM users WHERE id = ?', id)
-    connection.commit()
+    # connection.commit()
 
 
     return func.HttpResponse(f"User deleted successfully.")
--- shared/auth.py
+++ shared/auth.py
@@ -4,22 +4,22 @@ import azure.functions as func
 from enum import Enum
 
 
-class AuthLevels(Enum):
+class AuthLevels():
     GUEST = 0
     USER = 1
     ADMIN = 2
 
 
-def auth(req, auth_level=AuthLevels.GUEST):
+def auth(req, action, auth_threshold = AuthLevels.GUEST):
     token = req.headers.get("Bareer")
     auth_data = jwt.decode(token)
     # logging.warn(auth_data)
     # E.g: {'id': 1, 'username': 'FirstUser', 'is_admin': False}
 
-    if(auth_level > get_current_auth_level(auth_data)):
-        return func.HttpResponse(f"Unauthorized", status_code=403)
+    if(auth_threshold <= get_current_auth_level(auth_data)):
+        return action(auth_data)
 
-    return auth_data
+    return func.HttpResponse(f"Unauthorized", status_code=403)
 
 
 
