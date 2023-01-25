4baa1b1f4bf20c8405ee12f58a05fd4e4338c70b
Oskar Legner <oskarlegner@gmail.com>
2023-01-25 23:40:18 +0100

    Adjust auth logic to use bareer token

--- shared/auth.py
+++ shared/auth.py
@@ -3,7 +3,7 @@ from shared import db, jwt, cookie
 import azure.functions as func
 
 def auth(req):
-    token = cookie.read(req, "jwt")
+    token = req.headers.get("Bareer")
     auth_data = jwt.decode(token)
     logging.warn(auth_data)
     # {'id': 1, 'username': 'FirstUser', 'is_admin': False}
