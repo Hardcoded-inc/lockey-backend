90295c00bb416e6359a74626e938f9407abcd2e2
Oskar Legner <oskarlegner@gmail.com>
2023-01-25 23:56:35 +0100

    Add auth levels

--- DeleteUser/__init__.py
+++ DeleteUser/__init__.py
@@ -3,18 +3,15 @@ from shared import db
 import azure.functions as func
 import json
 from typing import Optional
-from shared.auth import auth
+from shared.auth import auth, AuthLevels
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     id = req.route_params.get('id')
     logging.info(f"Delete User (id: {id}) function processed a request.")
-    auth_data = auth(req)
+    auth_data = auth(req, AuthLevels.ADMIN)
 
-    if(auth_data and auth_data["is_admin"]):
-        return delete_user(id)
-    else:
-        return func.HttpResponse(f"Unauthorized", status_code=403)
+    return delete_user(id)
 
 
 
--- shared/auth.py
+++ shared/auth.py
@@ -1,11 +1,30 @@
 import logging
 from shared import db, jwt, cookie
 import azure.functions as func
+from enum import Enum
 
-def auth(req):
+
+class AuthLevels(Enum):
+    GUEST = 0
+    USER = 1
+    ADMIN = 2
+
+
+def auth(req, auth_level):
     token = req.headers.get("Bareer")
     auth_data = jwt.decode(token)
-    logging.warn(auth_data)
-    # {'id': 1, 'username': 'FirstUser', 'is_admin': False}
+    # logging.warn(auth_data)
+    # E.g: {'id': 1, 'username': 'FirstUser', 'is_admin': False}
+
+    if(auth_level > get_current_auth_level(auth_data)):
+        return func.HttpResponse(f"Unauthorized", status_code=403)
 
     return auth_data
+
+
+
+
+def get_current_auth_level(data):
+    if(not data): return AuthLevels.GUEST
+    if(data["is_admin"]): return AuthLevels.ADMIN
+    return AuthLevels.USER
